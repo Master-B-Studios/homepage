64604929f7be151931429eef6c941ded33c38e89
Master B <dubmaster.b@hotmail.com>
2025-02-19 20:18:07 +0100

    Update main.js

--- Local-Tools/Windows-Scripts/main.js
+++ Local-Tools/Windows-Scripts/main.js
@@ -1,7 +1,7 @@
 function download(obj) {
-  if (obj == 'no_shortcuts') {url = 'data/Windows 10 Shortcuts Manager.cmd'}
-  else if (obj == 'booklet') {url = 'files/booklet/booklet_pronto_script.pdf'}
-  else if (obj == 'skin') {url = 'files/skin/MB-Pronto.mxcf'}
+  if (obj == 'no_shortcuts') {url = 'data/Windows 10 Shortcuts Manager'}
+  else if (obj == 'win_11_context') {url = 'data/Windows 11 Context-Menu Manager.cmd'}
+  else if (obj == 'scroll_manager') {url = 'data/WinOnMac Scrolling Manager.cmd'}
   var link = document.createElement("a");
   link.href = url;
   link.download = url.split('/').pop();
