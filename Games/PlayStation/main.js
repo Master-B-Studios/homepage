0370c4902e7c762aa7b42437f4eabbe38de98af5
Master B <dubmaster.b@hotmail.com>
2025-02-27 13:24:02 +0100

    Added PS5 UMTX

--- Games/PlayStation/main.js
+++ Games/PlayStation/main.js
@@ -1,11 +1,9 @@
 function download(obj) {
-    if (obj == 'mb3pwn') {url = 'files/app/MB-3Pwn.apk'}
-    else if (obj == 'booklet') {url = 'files/booklet/booklet_pronto_script.pdf'}
-    else if (obj == 'skin') {url = 'files/skin/MB-Pronto.mxcf'}
-    var link = document.createElement("a");
-    link.href = url;
-    link.download = url.split('/').pop();
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
+  if (obj == 'mb3pwn') {url = 'PS4 - 11.00 Jailbreak/app/MB-3Pwn.apk'}
+  var link = document.createElement("a");
+  link.href = url;
+  link.download = url.split('/').pop();
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
