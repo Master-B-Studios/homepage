d852ab1dcbaa958da99d8aa6f17295285bf6f1ef
Master B <dubmaster.b@hotmail.com>
2025-04-01 15:51:57 +0200

    updates

--- Local-Tools/Android Basics/main.js
+++ Local-Tools/Android Basics/main.js
@@ -3,6 +3,8 @@ function download(obj) {
   else if (obj == 'tc_smb') {url = 'files/apps/Total Commander SMB-Plugin.apk'}
   else if (obj == 'magisk') {url = 'files/apps/Magisk 27.apk'}
   else if (obj == 'magisk_rec') {url = 'files/apps/Magisk 27.zip'}
+  else if (obj == 'app_deb') {url = 'files/apps/Applications Debugger 3.3.5.09.apk'}
+  else if (obj == 'app_info') {url = 'files/apps/Applications Info 1.6.apk'}
   var link = document.createElement("a");
   link.href = url;
   link.download = url.split('/').pop();
