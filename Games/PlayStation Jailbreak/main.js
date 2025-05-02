02fba4160ea880c12dd2630b142da95dcdddee23
Master B <dubmaster.b@hotmail.com>
2025-05-03 00:25:55 +0200

    updates

--- Games/PlayStation Jailbreak/main.js
+++ Games/PlayStation Jailbreak/main.js
@@ -2,7 +2,7 @@ function download(obj) {
   if (obj == 'mb3pwn') {url = 'downloads/PS4 MB-3Pwn.apk'}
   else if (obj == 'ps5_jb_05.10') {url = 'downloads/PS5 Jailbreak 05.10 - MBMB00510.pkg'}
   else if (obj == 'psender') {url = 'downloads/MB PSender 1.0.apk'}
-  else if (obj == 'eta2.0_kstuff') {url = 'downloads/PS5 etaHEN 2.0b kstuff 5.10.bin'}
+  else if (obj == 'eta2.1') {url = 'downloads/PS5 etaHEN 2.1.bin'}
   var link = document.createElement("a");
   link.href = url;
   link.download = url.split('/').pop();
